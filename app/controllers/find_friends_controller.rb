a7d83eaaf84c6a83dc0bb7dff8ac5f9912db2ae2
sugiarto <ugidmtest@gmail.com>
2018-08-27 08:01:08 +0700

    X fix bug find friends

--- app/controllers/find_friends_controller.rb
+++ app/controllers/find_friends_controller.rb
@@ -3,7 +3,8 @@ class FindFriendsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.where("id not in (?) AND id != ?", current_user.friend_ids, current_user).paginate(page: params[:page])
+    friends_ids = current_user.friend_ids.empty?? 'x' : current_user.friend_ids
+    @users = User.where("id not in (?) AND id != ?", friends_ids, current_user).paginate(page: params[:page])
     respond_to do |format|
       format.js
       format.html
